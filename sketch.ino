58a8bd54a727ab8b41779786ca70dfec34be1337
Naman Sharma <namansharma232009@gmail.com>
2022-12-05 18:07:41 +0530

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,16 @@
+#include<Servo.h>
+Servo servo;
+void setup() {
+  servo.attach(8);
+  Serial.begin(9600);
+}
+
+void loop() {
+  while(Serial.available() > 0){
+  i = Serial.readString().toInt();
+  servo.write(i);
+  Serial.println(i);
+  delay(500);
+  servo.write(0);
+  }
+}
